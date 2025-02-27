7b59b6c7730557ccad2344228bb925c05d425d1f
Zichong Chen <chenzichong01@gmail.com>
2025-02-27 23:54:06 +0800

    Slight improvements in the Gradio demo.

--- app.py
+++ app.py
@@ -13,7 +13,6 @@ from webui import (
     create_interface_style_t2i,
     create_interface_style_transfer
 )
-# from utils import memory_efficient, Controller
 from utils import Controller
 
 import os
@@ -28,25 +27,16 @@ class Runner:
     
     def load_pipeline(self, model_path_or_name):
 
-        # DELETE IT
-        if 'xl' in model_path_or_name:
-            model_path_or_name = r'E:\research\checkpoints\diffusion\huggingface\stable-diffusion-xl-base-1.0'
-        else:
-            model_path_or_name = r'E:\research\checkpoints\diffusion\huggingface\stable-diffusion-v1-5'
-
-
-
-
         self.model_name = model_path_or_name 
         
         scheduler = DDIMScheduler.from_pretrained(model_path_or_name, subfolder="scheduler")
         pipe_class = ADXLPipeline if 'xl' in model_path_or_name else ADPipeline
         self.pipeline = pipe_class.from_pretrained(model_path_or_name, scheduler=scheduler, safety_checker=None)
         self.pipeline.classifier = self.pipeline.unet
-        # memory_efficient(self.pipeline)
 
-    def preprocecss(self, image: Image.Image):
-        width, height = image.size
+    def preprocecss(self, image: Image.Image, height=None, width=None):
+        if width is None or height is None: 
+            width, height = image.size
         new_width = (width // 64) * 64
         new_height = (height // 64) * 64
         size = (new_width, new_height)
@@ -58,16 +48,14 @@ class Runner:
             self.load_pipeline(model) 
 
         content_image = self.preprocecss(content_image)
-        style_image = self.preprocecss(style_image)
+        style_image = self.preprocecss(style_image, height=512, width=512)
 
         height, width = content_image.shape[-2:]
         set_seed(seed)
         controller = Controller(self_layers=(10, 16))
         result = self.pipeline.optimize(
-            # attn_scale=1,
             lr=lr,
             batch_size=1,
-            # loss_fn=self.loss_fn,
             iters=1,
             width=width,
             height=height,
@@ -77,15 +65,19 @@ class Runner:
             content_image=content_image,
             mixed_precision=mixed_precision,
             num_inference_steps=num_steps,
+            enable_gradient_checkpoint=False,
         )
         output_image = to_pil_image(result[0])
+        del result
+        torch.cuda.empty_cache()
         return [output_image]
 
     def run_style_t2i_generation(self, style_image, prompt, negative_prompt, guidance_scale, height, width, seed, num_steps, iterations, lr, num_images_per_prompt, mixed_precision, is_adain, model):
         if self.pipeline is None or self.model_name != model:
             self.load_pipeline(model)
 
-        style_image = self.preprocecss(style_image)
+        height, width = (1024, 1024) if 'xl' in self.model_name else (512, 512)
+        style_image = self.preprocecss(style_image, height=height, width=width)
 
         set_seed(seed)
         if isinstance(self.pipeline, StableDiffusionPipeline):
@@ -107,20 +99,22 @@ class Runner:
             style_image=style_image,
             prompt=prompt,
             negative_prompt=negative_prompt,
-            # loss_fn=self.loss_fn,
             guidance_scale=guidance_scale,
             num_inference_steps=num_steps,
             num_images_per_prompt=num_images_per_prompt,
             enable_gradient_checkpoint=False
         )
         output_images = [to_pil_image(image) for image in images]
+
+        del images
+        torch.cuda.empty_cache()
         return output_images
 
     def run_texture_synthesis(self, texture_image, height, width, seed, num_steps, iterations, lr, mixed_precision, num_images_per_prompt, synthesis_way,model):
         if self.pipeline is None or self.model_name != model:
             self.load_pipeline(model) 
 
-        texture_image = self.preprocecss(texture_image)
+        texture_image = self.preprocecss(texture_image, height=512, width=512)
 
         set_seed(seed)
         controller = Controller(self_layers=(10, 16))
@@ -128,9 +122,7 @@ class Runner:
         if synthesis_way == 'Sampling':
             results = self.pipeline.sample(
                 lr=lr,
-                # scale=1.,
                 adain=False,
-                # loss_fn=self.loss_fn,
                 iters=iterations,
                 width=width,
                 height=height,
@@ -149,7 +141,6 @@ class Runner:
         elif synthesis_way == 'MultiDiffusion':   
             results = self.pipeline.panorama(
                 lr=lr,
-                # loss_fn=self.loss_fn,
                 iters=iterations,
                 width=width,
                 height=height,
@@ -171,6 +162,8 @@ class Runner:
             raise ValueError
         
         output_images = [to_pil_image(image) for image in results]
+        del results
+        torch.cuda.empty_cache()
         return output_images
 
 
